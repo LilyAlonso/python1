be7e27b24cd17c82c9f31c1dc9b5c136b59dfb8f
LilyAlonso <lily.ag94@gmail.com>
2016-02-08 10:53:27 -0600

    wsq03

---
+++ numeros.py
@@ -0,0 +1,10 @@
+num1=int(input("Escribe un número: "))
+num2=int(input("Escribe otro número: "))
+resta=num1-num2
+mult=num1*num2
+div=num1//num2
+resd=num1%num2
+print ("La resta de los números es: ", resta)
+print ("La multiplicación de los números es: ", mult)
+print ("La división de los números es: ", div)
+print ("El residuo es: ", resd)
