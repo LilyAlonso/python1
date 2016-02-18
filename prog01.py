b30cea2b5fe0734182649f3094f95f7c3e74abed
LilyAlonso <lily.ag94@gmail.com>
2016-02-18 09:23:38 -0600

    quiz02

---
+++ prog01.py
@@ -0,0 +1,7 @@
+def superpower(x,y):
+    z=x**y
+    return z
+
+x=5
+y=3
+print (superpower(x,y))
---
+++ prog02.py
@@ -0,0 +1,6 @@
+def stars(a):
+    b=""
+    for i in range(a):
+        b+="*"
+    print(b)
+stars(8)
