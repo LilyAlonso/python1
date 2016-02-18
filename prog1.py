3446bd7bd4995d87c69bbed5115f3e4e2ac62d21
LilyAlonso <lily.ag94@gmail.com>
2016-02-18 08:58:14 -0600

    quiz01

---
+++ prog1.py
@@ -0,0 +1,4 @@
+h = float(input("Escribe el valor de la altura: "))
+r = float(input("Escribe el valor del radio: "))
+v = 3.1416*r*r*h
+print("El volúmen del cilindro es: ",v)
---
+++ prog2.py
@@ -0,0 +1,8 @@
+num1 = int(input("Escribe el primer número: "))
+num2 = int(input("Escribe el segundo número: "))
+mul = num1*num2
+div = num1//num2
+res = num1%num2
+print("El producto de los números es: ",mul)
+print("La división de los números es: ",div)
+print("El residuo de la división de los números es: ",res)
