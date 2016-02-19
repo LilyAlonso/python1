72ac2f084850b04344d56f39172c5acf6b1dcd06
LilyAlonso <lily.ag94@gmail.com>
2016-02-19 13:33:07 -0600

    quiz01

---
+++ prog3.py
@@ -0,0 +1,10 @@
+num1 = float(input("Escribe el primer numero: "))
+num2 = float(input("Escribe el segundo numero: "))
+suma = num1+num2
+rest = num1-num2
+mul = num1*num2
+div = num1/num2
+print("La suma de los numeros es: ",suma)
+print("La resta de los numeros es: ",rest)
+print("El producto de los numeros es: ",mul)
+print("La division de los numeros es: ",div)
